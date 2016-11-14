b6ffa46e34045aedf59462947ded27e7eb78136a
Peter Bijker <peter.bijker@gmail.com>
2016-11-15 00:47:46 +0100

    Created index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Samenweb</title>
+</head>
+<body>
+    <p>Hallo beste 2MD, we gaan samen een nieuw project via Git maken.</p>
+    <p>Je maakt een nieuwe branch van dit project en doet een aanpassing.</p>
+    <p>Het is het jaar: <?php echo date('Y'); ?></p>
+</body>
+</html>
