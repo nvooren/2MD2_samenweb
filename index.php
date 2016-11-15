4e34ac8e7c4dc0e86670bd215ee77dc8c3edde00
nvooren <nick@yvonnevooren.nl>
2016-11-15 09:04:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
     <p>Hallo beste 2MD, we gaan samen een nieuw project via Git maken.</p>
     <p>Je maakt een nieuwe branch van dit project en doet een aanpassing.</p>
     <p>Het is het jaar: <?php echo date('Y'); ?></p>
+    <p> ik heb ook wat gedaan!<p>
 </body>
 </html>
